9bd1052af17b30f74a4624b4ef989910e2d119d4
sanjae <sachinxsan@gmail.com>
2024-06-11 10:23:15 -0400

    Attempted Generating random maze v1

--- code/generateMaze.ts
+++ code/generateMaze.ts
@@ -21,27 +21,6 @@ interface NodeM{
     walls?: number[];
 }
 
-function getRandomDirection(node: NodeM){
-    let tempNode: NodeM;
-
-    while(!tempNode){
-        let index: number = Math.floor(Math.random()*4);
-        tempNode = node.directions[index];
-        if(!tempNode.visited){
-            let tempIndex: number = node.walls.indexOf((index+2)%4);
-            let tempLength: number = node.walls.length -1;
-            if(tempIndex != tempLength){
-                let holder: number = node.walls[]
-            }
-
-        }
-    }
-    
-    
-
-    return tempNode;
-}
-
 function intializeMazeNodes(maze: MazeM){
     let row: number =  maze.height;
     let col: number = maze.width;
@@ -140,10 +119,90 @@ function connectNodes(maze: MazeM, curNode: NodeM, location: number){
     curNode.visited = false;
 }
 
-function makePaths(maze: MazeM, curNode: NodeM){
-    let randomDirection: NodeM = getRandomDirection(curNode);
+function getRandomDirection(node: NodeM){
+    let tempNode: NodeM;
 
-    makePaths(maze, randomDirection);
+    while(!tempNode){
+        let index: number = Math.floor(Math.random()*4);
+        tempNode = node.directions[index];
+        if(!tempNode.visited){
+            let tempIndex: number = node.walls.indexOf((index+2)%4);
+            let lastIndex: number = node.walls.length -1;
+            if(tempIndex != lastIndex){
+                let holder: number = node.walls[lastIndex];
+                node.walls[lastIndex] = node.walls[tempIndex];
+                node.walls[tempIndex] = holder;
+            }
+            node.walls.pop();
+
+            tempIndex = tempNode.walls.indexOf((index+2)%4);
+            lastIndex = tempNode.walls.length -1;
+            if(tempIndex != lastIndex){
+                let holder: number = tempNode.walls[lastIndex];
+                tempNode.walls[lastIndex] = tempNode.walls[tempIndex];
+                tempNode.walls[tempIndex] = holder;
+            }
+            tempNode.walls.pop();
+        }
+    }
+    
+    return tempNode;
+}
+
+function makePaths(maze: MazeM, curNode: NodeM){
+    curNode.visited = true;
+    let north: boolean;
+    let east: boolean;
+    let south: boolean;
+    let west: boolean;
+
+    do{
+        let randomDirection: NodeM = getRandomDirection(curNode);
+        makePaths(maze, randomDirection);
+        
+        north = !curNode.directions[0].visited;
+        east = !curNode.directions[1].visited;
+        south = !curNode.directions[2].visited;
+        west = !curNode.directions[3].visited;
+    }while(north && east && south && west)
+}
+
+function toString(maze: MazeM){
+    let nodes: Array<NodeM> = maze.nodes;
+    let row : number = maze.height;
+    let col : number = maze.width;
+
+    let retString: string = "";
+    let northSouth: string = "---";
+    let noSouth: string = "  ";
+    let eastWest: string = "| ";
+    let noEast: string = " "
+
+    retString += northSouth.repeat(col) + "\n";
+    for(let i = 0; i < row; i++){
+        let southString: string = "";
+        retString += eastWest;
+        for(let j = 0; j < col; j++){
+            let node = nodes[i*col+j];
+            let index = node.walls.indexOf(Directions.East);
+
+            if(index != -1){
+                retString += eastWest;
+            }else{
+                retString += noEast;
+            }
+
+            index = node.walls.indexOf(Directions.South);
+            
+            if(index != -1){
+                southString += northSouth;
+            }else{
+                southString += noSouth;
+            }
+        }
+        retString += eastWest + "\n" + southString + "\n";
+    }
+    retString += northSouth.repeat(col);
 }
 
 let maze: MazeM = {height: 3, width: 3};
